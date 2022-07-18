811f306cbb31b1d34de6c848d1d0f1a3731aecbb
Gokul-C <gokulc741@gmail.com>
2022-07-18 22:24:49 +0530

    first commit

---
+++ git.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+git init
+git remote add origin https://github.com/Gokul-C/ansible
+git config --global user.email "gokulc741@gmail.com"
+git config --global user.name "Gokul-C"
+git checkout -b main
+git add .
+git commit -m "first commit"
+
